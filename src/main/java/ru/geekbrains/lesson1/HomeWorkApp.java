6e69ca439cc32f3d1f147652f708cc11dd6d5a2e
EvgeniyKurlyanchik <91161381+EvgeniyKurlyanchik@users.noreply.github.com>
2021-09-25 21:02:05 +0300

    Initial commit

--- src/main/java/ru/geekbrains/lesson1/HomeWorkApp.java
+++ src/main/java/ru/geekbrains/lesson1/HomeWorkApp.java
@@ -28,7 +28,7 @@ public static void printColor(int value) {
 
         if (value <= 0) {
         System.out.println("Красный");
-        } else if (value > 0 && value <= 100) {
+        } else if ((value > 0) && value <= 100) {
         System.out.println("Жёлтый");
         } else {
         System.out.println("Зеленый ");
