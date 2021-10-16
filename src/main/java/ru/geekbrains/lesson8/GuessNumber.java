5d9c1ad18139ac4f33dc9e0d9a627b3d0a2d8dd3
EvgeniyKurlyanchik <91161381+EvgeniyKurlyanchik@users.noreply.github.com>
2021-10-16 15:42:34 +0300

    Initial commit

--- src/main/java/ru/geekbrains/lesson8/GuessNumber.java
+++ src/main/java/ru/geekbrains/lesson8/GuessNumber.java
@@ -8,43 +8,54 @@ import javax.swing.*;
 
 public class GuessNumber extends JFrame {
     private  int randomNumber;
-
+    private JTextField textField2;
     private JTextField textField;
    public GuessNumber() {
-       randomNumber = (int)(Math.random() * 10) + 1;
+       randomNumber = (int) (Math.random() * 10) + 1;
        //create Window
        setTitle("Угадай число");
-       setBounds(600,300,600,140);
+       setBounds(600, 300, 600, 140);
        setDefaultCloseOperation(EXIT_ON_CLOSE);
        setResizable(false);
        // текстовое поле
-        textField = new JTextField();
-        add(textField, BorderLayout.SOUTH);
-        textField.setText(" Отдагайте число от 1 до 10");
-        textField.setEditable(false);
-        textField.setHorizontalAlignment(SwingConstants.CENTER);
-        Font font = new Font("ArialBlack",Font.ITALIC, 16);
-        textField.setFont(font);
-        //панель кнопок
-       JPanel buttonsPanel = new JPanel(new GridLayout(1,10));
+       textField = new JTextField();
+       add(textField, BorderLayout.SOUTH);
+       textField.setText(" Отдагайте число от 1 до 10");
+       textField.setEditable(false);
+       textField.setHorizontalAlignment(SwingConstants.CENTER);
+       Font font = new Font("ArialBlack", Font.ITALIC, 16);
+       textField.setFont(font);
+       //панель кнопок
+       JPanel buttonsPanel = new JPanel(new GridLayout(1, 10));
        add(buttonsPanel, BorderLayout.CENTER);
        for (int i = 1; i <= 10; i++) {
            JButton button = new JButton(String.valueOf(i));
            button.setFont(font);
            buttonsPanel.add(button);
-           int buttonIndex =i;
+           int buttonIndex = i;
            button.addActionListener(new ActionListener() {
                @Override
                public void actionPerformed(ActionEvent e) {
 
-               tryToAnswer(buttonIndex,button);
+                   tryToAnswer(buttonIndex, button);
                }
            });
 
+           setVisible(true);
        }
-
-
+       JButton button11 = new JButton();
+       add(button11, BorderLayout.NORTH);
+       button11.setText("Restart");
+       button11.setFont(font);
+       setResizable(false);
        setVisible(true);
+       button11.addActionListener(new ActionListener() {
+           @Override
+           public void actionPerformed(ActionEvent e) {
+               new GuessNumber();
+           }
+
+       });
    }
 
     public  void tryToAnswer ( int answer, JButton button){
