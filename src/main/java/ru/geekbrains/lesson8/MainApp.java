335227c8cabc64ef002e42b9c0c556e5f7befb05
EvgeniyKurlyanchik <91161381+EvgeniyKurlyanchik@users.noreply.github.com>
2021-10-16 23:52:14 +0300

    Initial commit

--- src/main/java/ru/geekbrains/lesson8/MainApp.java
+++ src/main/java/ru/geekbrains/lesson8/MainApp.java
@@ -1,11 +1,14 @@
 package ru.geekbrains.lesson8;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class MainApp {
+
     public static void main(String[] args) {
-      //  new MyWindow();
-        new GuessNumber();
+        //  new MyWindow();
+
+        GuessNumber guessNumber = new GuessNumber();
 
     }
-
-
 }
